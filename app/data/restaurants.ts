af3b148124ac555cc0caac8fe0f618f2eff0c119
BlapperSmacks <levishields56@gmail.com>
2024-10-10 17:22:06 -0400

    Update restaurants.ts

---
+++ app/data/restaurants.ts
@@ -0,0 +1,12 @@
+export const restaurants = [
+  { name: "McDonald's", logo: "/logos/mcdonalds.png" },
+  { name: "Burger King", logo: "/logos/burgerking.png" },
+  { name: "Wendy's", logo: "/logos/wendys.png" },
+  { name: "Subway", logo: "/logos/subway.png" },
+  { name: "Taco Bell", logo: "/logos/tacobell.png" },
+  { name: "KFC", logo: "/logos/kfc.png" },
+  { name: "Chick-fil-A", logo: "/logos/chickfila.png" },
+  { name: "Domino's", logo: "/logos/dominos.png" },
+  { name: "Pizza Hut", logo: "/logos/pizzahut.png" },
+  { name: "Chipotle", logo: "/logos/chipotle.png" },
+]
